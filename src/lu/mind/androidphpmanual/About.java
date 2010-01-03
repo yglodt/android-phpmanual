ea2824ab1e5abcd431331548ab26152f0eecf03f
yglodt <yglodt@033f9c74-f39c-11de-9907-777483041811>
2010-01-03 21:05:24 +0000

    re-import to remove the unneeded /trunk folder

---
+++ src/lu/mind/androidphpmanual/About.java
@@ -0,0 +1,26 @@
+package lu.mind.androidphpmanual;
+
+
+import android.app.Activity;
+import android.os.Bundle;
+import lu.mind.androidphpmanual.R;
+
+public class About extends Activity {
+
+	//private Button closeButton;
+	
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+    	super.onCreate(savedInstanceState);
+    	setContentView(R.layout.about);
+    	
+    	/*closeButton = (Button) this.findViewById(R.id.closeAbout);
+    	closeButton.setOnClickListener(new OnClickListener() {
+			@Override
+			public void onClick(View v) {
+				finish();
+			}
+    	});
+    	*/
+    }
+}
